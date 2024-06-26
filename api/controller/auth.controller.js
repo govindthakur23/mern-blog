db1f093c8599be0547b29ef9e39dd92ea2c09c43
govindthakur23 <montygovind9@gmail.com>
2024-06-26 14:10:56 +0530

    complete sign Up page ui

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,26 +1,25 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../util/error.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../util/error.js";
 
 export const signup = async (req, res, next) => {
-
   const { username, email, password } = req.body;
-  if(!username || !password || !email || username === '' || email === '' || password === ''){
-     next(errorHandler(400,'All fields are required'));  
-}
+  if (!username || !password || !email || username === "" ||email === "" || password === "" ) {
+    next(errorHandler(400, "All fields are required"));
+  }
 
-    const hashedPassword = bcryptjs.hashSync(password,10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const  newUser =  new User({
-        username:username,
-        email:email,
-        password:hashedPassword,
-    });
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
 
-    try {
-        await newUser.save();
-        res.json('signup Successful')
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await newUser.save();
+    res.json("signup Successful");
+  } catch (error) {
+    next(error);
+  }
 };
